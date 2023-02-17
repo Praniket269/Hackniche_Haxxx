aeb038d80e54cca4840e4d000cb2b791ffe22f1c
Siddhi Muni <siddhimuni1302@gmail.com>
2023-02-17 15:42:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import './App.css';
-import {MainPage} from './MyComponents/LandingPage/MainPage'
-
-
+import React from 'react'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './MyComponents/Login';
+import Signup from './MyComponents/Signup';
 function App() {
   return (
-    <>
-      <MainPage/>
-    </>
-  );
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                {/* <li className="nav-item">
+                  <Link className="nav-link" to='/login'>
+                    Login
+                  </Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/sign-up" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
+export default App
