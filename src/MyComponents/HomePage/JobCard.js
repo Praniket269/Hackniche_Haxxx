4e71a22cf10735262408a802e3d6ef97487ac40e
talkshrey <talkshrey@gmail.com>
2023-02-18 01:48:15 +0530

    changes made

--- src/MyComponents/HomePage/JobCard.js
+++ src/MyComponents/HomePage/JobCard.js
@@ -1,21 +1,27 @@
 export default function JobCard() {
   return (
+    <div>
       <div className="row px-3">
         <div className="card col mx-2">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 className="card-title">Chief Security Officer</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Serving Skill</h6>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Having clear ability to deliver high-quality, cost-effective IT
+              solutions that serve the strategic and business needs of the
+              organization
             </p>
             <p>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 1</span>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 2</span>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 3</span>
+              <span className="badge rounded-pill bg-primary mx-1">Army</span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Automation
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Vigilance
+              </span>
             </p>
-            <div>
-              <button type="button" className="btn btn-primary">
+            <div className="my-2">
+              <button type="button" className="btn btn-secondary text-black">
                 Apply
               </button>
             </div>
@@ -23,24 +29,88 @@ export default function JobCard() {
         </div>
         <div className="card col mx-2">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 className="card-title">Chief Information Security Officer</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Metropolitan Stock Exchange
+            </h6>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              To identify information security goals/ objectives and also define
+              the scope and boundaries of the information security and cyber
+              security program.
             </p>
             <p>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 1</span>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 2</span>
-              <span className="badge rounded-pill bg-primary mx-1">Tag 3</span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                IT Security
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Framework
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">RMC</span>
             </p>
-            <div>
-              <button type="button" className="btn btn-primary">
+            <div className="my-2">
+              <button type="button" className="btn btn-secondary text-black">
                 Apply
               </button>
             </div>
           </div>
         </div>
       </div>
+      <div className="row px-3 my-2">
+        <div className="card col mx-2">
+          <div className="card-body">
+            <h5 className="card-title">Chief Security Officer</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Serving Skill</h6>
+            <p className="card-text">
+              Having clear ability to deliver high-quality, cost-effective IT
+              solutions that serve the strategic and business needs of the
+              organization
+            </p>
+            <p>
+              <span className="badge rounded-pill bg-primary mx-1">Army</span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Automation
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Vigilance
+              </span>
+            </p>
+            <div className="my-2">
+              <button type="button" className="btn btn-secondary text-black">
+                Apply
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card col mx-2">
+          <div className="card-body">
+            <h5 className="card-title">Chief Information Security Officer</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              Metropolitan Stock Exchange
+            </h6>
+            <p className="card-text">
+              To identify information security goals/ objectives and also define
+              the scope and boundaries of the information security and cyber
+              security program To plan and establish organization-wide
+              Information security Management System (ISMS) in accordance with
+              ISO/IEC 27001.
+            </p>
+            <p>
+              <span className="badge rounded-pill bg-primary mx-1">
+                IT Security
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">
+                Framework
+              </span>
+              <span className="badge rounded-pill bg-primary mx-1">RMC</span>
+            </p>
+            <div className="my-2">
+              <button type="button" className="btn btn-secondary text-black">
+                Apply
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
