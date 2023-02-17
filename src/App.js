eb34b7aebfb61f2fc6ddde07fb4344a4a37a6177
talkshrey <talkshrey@gmail.com>
2023-02-17 15:56:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import JobPage from "./MyComponents/HomePage/JobPage";
 import { Navbar } from "./MyComponents/HomePage/Navbar";
 import { MainPage } from "./MyComponents/LandingPage/MainPage";
+import {NewNav} from "./MyComponents/HomePage/NewNav";
 
 function App() {
   return (
     <>
       {/* <MainPage /> */}
       {/* <Navbar/> */}
+      <NewNav />
       <JobPage />
     </>
   );
--- src/MyComponents/HomePage/NewNav.js
+++ src/MyComponents/HomePage/NewNav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './NewNav.css'
-import navlogo from '../LandingPage/logo.png'
+import navlogo from '../Assets/logo.png'
 
 export const NewNav = () => {
   return (
@@ -8,8 +8,8 @@ export const NewNav = () => {
     <div className="mynav">
         <div className= "navLogo"><img src={navlogo} alt="" /></div>
         <div className="featureIcons">
-            <a className= "icons" href="">Jobs</a>
-            <a className= "icons" href="">Education</a>
+            <a className= "icons" href="/jobs">Jobs</a>
+            <a className= "icons" href="/education">Education</a>
             <a className= "icons" href="">Pension</a>
             <a className= "icons" href="">Community</a>
             <a className= "icons" href="">Schemes</a>
