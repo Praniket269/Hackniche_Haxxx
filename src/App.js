fd63f2c9ac4a568d82beb844c5cbaa51337b2793
talkshrey <talkshrey@gmail.com>
2023-02-18 07:42:42 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             </Routes>
           </div>
         </div>
-      </Router>
+      </Router> 
     </>
   );
 }
--- src/MyComponents/Community/Community.js
+++ src/MyComponents/Community/Community.js
@@ -27,8 +27,8 @@ export default function Community() {
         </div>
         <div className="col-9 px-0">
           <div className="container">
-            <h3 className="my-2"> Tier 1 </h3>
-            <div className="row g-2">
+            <h1 className="my-3 py-4"> Government Schemes for Servicemen </h1>
+            {/* <div className="row g-2">
               {[0, 1].map((item) => (
                 <div key={item} class="col-6">
                   <div className="p-3 border bg-light">
@@ -36,9 +36,45 @@ export default function Community() {
                   </div>
                 </div>
               ))}
-            </div>
-            <h3 className="my-2"> Tier 2 </h3>
-            <div class="row g-2">
+            </div> */}
+            <div className="App1 my-1">
+      <table>
+        <tr>
+          <th>Schemes provided</th>
+          <th>beneficiary Amount</th>
+        </tr>
+        <tr>
+          <td>Penury Grant (65 Yrs)</td>
+          <td>4,000/-pm</td>
+        </tr>
+        <tr>
+          <td>Education Grant</td>
+          <td>Rs.1000/- per month per head for the previous academic year. Payable in one instalment in a financial year. </td>
+        </tr>
+        <tr>
+          <td>Officer Cadet Grant </td>
+          <td>Rs.1,000/- per month per child of an eligible ESM/widow. Payable in one instalment, on successful completion of two terms in a year. </td>
+        </tr>
+        <tr>
+          <td>Disabled Children Grant</td>
+          <td>Rs 3,000/- per month, payable on monthly basis.</td>
+        </tr>
+        <tr>
+          <td>Daughter’s Marriage and Widow Re-Marriage Grant (02 Daughters)</td>
+          <td>50,000/-</td>
+        </tr>
+        <tr>
+          <td>Medical Grant</td>
+          <td>30,000/- (Max)</td>
+        </tr>
+        <tr>
+          <td>Penury Grant (65 Yrs)</td>
+          <td>4,000/-pm</td>
+        </tr>
+      </table>
+    </div>
+            {/* <h3 className="my-2"> Tier 2 </h3> */}
+            {/* <div class="row g-2">
               {[0, 1, 2, 3].map((item) => (
                 <div className="col-6" key={item}>
                   <div className="p-3 border bg-light">
@@ -46,7 +82,7 @@ export default function Community() {
                   </div>
                 </div>
               ))}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
